77e63c7c004539e55634a9406d1365a148e8ba0d
Alexandre Serex <alexandre.serex@he-arc.ch>
2016-10-19 15:11:58 +0200

    beggining to play with views

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <title>Contacts</title>
+</head>
+
+<body>
+<div id="'main-wrapper">
+    @yield('content')
+</div>
+</body>
+</html>
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('content')
+
+<h1>Contact Me : {{ $name }} {{ $lastname }}</h1>
+
+@stop
