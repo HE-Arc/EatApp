730834ac9d693063eb2998472c49fe6aaaef921d
Alexandre Serex <alexandre.serex@he-arc.ch>
2016-11-30 21:29:12 +0100

    Creation of lists is now procedural based on a mockup of the databse output. Names of lists will have to be sluggable to create HTML-valid Ids.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,6 +19,27 @@ class HomeController extends Controller
     public function home()
     {
         $titre = 'Liste personnelle';
-        return view('layouts.children.children.index', compact('titre'));
+        $listesIng = [
+            'Liste_Maison' => [
+                'name' => 'Liste Maison',
+                'ingredients' => [
+                    ['desc' => 'Poulet', 'quantity' => 300, 'unit' => 'gr'],
+                    ['desc' => 'Poisson', 'quantity' => 2, 'unit' => 'filet'],
+                    ['desc' => 'PQ', 'quantity' => 100, 'unit' => 'rouleaux']
+                ]
+            ],
+            'Liste_coloc' => [
+                'name' => 'Liste #coloc',
+                'ingredients' => [
+                    ['desc' => 'Liquide vaisselle', 'quantity' => 1, 'unit' => 'bouteille'],
+                    ['desc' => 'Bière', 'quantity' => 20, 'unit' => 'l']
+                ]
+            ],
+            'Liste_nulle' => [
+                'name' => 'Liste nulle',
+                'ingredients' => []
+            ]
+        ];
+        return view('layouts.children.children.index', compact('titre', 'listesIng'));
     }
 }
--- resources/views/layouts/children/children/index.blade.php
+++ resources/views/layouts/children/children/index.blade.php
@@ -4,191 +4,49 @@
 
     <div class="row">
         <div class="panel-group full-width" id="accordion">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="button-justify">
-                            <span class="glyphicon glyphicon-folder-close"></span>
-                            Content</a>
-                    </h4>
-                </div>
-                <div id="collapseOne" class="panel-collapse collapse in">
-                    <div class="panel-body">
-                        <table class="table">
-                            <tr>
-                                <td class="ingredient-description">
-                                    <span class="glyphicon glyphicon-pencil text-primary"></span>POULET
-                                </td>
-                                <td class="ingredient-quantity">
-                                    <input type="number" min="0" max="100" />
-                                </td>
-                                <td class="ingredient-unit">
-                                    unité
-                                </td>
-                                <td class="ingredient-delete">
-                                    <span class="glyphicon glyphicon-trash text-danger"></span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span class="glyphicon glyphicon-flash text-success"></span>SANDWICH
-                                </td>
-                                <td class="ingredient-quantity">
-                                    <input type="number" min="0" max="100" />
-                                </td>
-                                <td class="ingredient-unit">
-                                    unité
-                                </td>
-                                <td class="ingredient-delete">
-                                    <span class="glyphicon glyphicon-trash text-danger"></span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span class="glyphicon glyphicon-file text-info"></span>CORNICHON
-                                </td>
-                                <td class="ingredient-quantity">
-                                    <input type="number" min="0" max="100" />
-                                </td>
-                                <td class="ingredient-unit">
-                                    unité
-                                </td>
-                                <td class="ingredient-delete">
-                                    <span class="glyphicon glyphicon-trash text-danger"></span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span class="glyphicon glyphicon-comment text-success"></span>PQ ET VITE SVP
-                                    <span class="badge">42</span>
-                                </td>
-                                <td class="ingredient-quantity">
-                                    <input type="number" min="0" max="100" />
-                                </td>
-                                <td class="ingredient-unit">
-                                    unité
-                                </td>
-                                <td class="ingredient-delete">
-                                    <span class="glyphicon glyphicon-trash text-danger"></span>
-                                </td>
-                            </tr>
-                        </table>
+            @forelse($listesIng as $id => $info)
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#{{$id}}" class="button-justify">
+                                <span class="glyphicon glyphicon-list"></span>
+                                {{$info['name']}}</a>
+                        </h4>
+                    </div>
+
+                    <div id="{{$id}}" class="collapse">
+                        <div class="panel-body">
+                            <table class="table">
+                                @forelse($info['ingredients'] as $ing)
+                                    <tr>
+                                        <td class="ingredient-description">
+                                            <span class="glyphicon glyphicon-apple text-primary"></span>
+                                            <label for="{{$ing['desc']}}_input"> {{$ing['desc']}}</label>
+                                        </td>
+                                        <td class="ingredient-quantity">
+                                            <input id="{{$ing['desc']}}_input" type="number" min="0" max="1000" class="full-width"
+                                                   value="{{$ing['quantity']}}"/>
+                                        </td>
+                                        <td class="ingredient-unit">
+                                            {{$ing['unit']}}
+                                        </td>
+                                        <td class="ingredient-delete">
+                                            <span class="glyphicon glyphicon-trash text-danger"></span>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <p>ADD AN INGREDIENT MAYBE</p>
+                                @endforelse
+                            </table>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="button-justify"><span
-                                    class="glyphicon glyphicon-th">
-                            </span>Modules</a>
-                    </h4>
-                </div>
-                <div id="collapseTwo" class="panel-collapse collapse">
-                    <div class="panel-body">
-                        <table class="table">
-                            <tr>
-                                <td>
-                                    <a href="http://www.jquery2dotnet.com">Orders</a> <span class="label label-success">$ 320</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="http://www.jquery2dotnet.com">Invoices</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="http://www.jquery2dotnet.com">Shipments</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="http://www.jquery2dotnet.com">Tex</a>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="button-justify"><span
-                                    class="glyphicon glyphicon-user">
-                            </span>Account</a>
-                    </h4>
-                </div>
-                <div id="collapseThree" class="panel-collapse collapse">
-                    <div class="panel-body">
-                        <table class="table">
-                            <tr>
-                                <td>
-                                    <a href="http://www.jquery2dotnet.com">Change Password</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="http://www.jquery2dotnet.com">Notifications</a> <span
-                                            class="label label-info">5</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="http://www.jquery2dotnet.com">Import/Export</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span class="glyphicon glyphicon-trash text-danger"></span><a
-                                            href="http://www.jquery2dotnet.com" class="text-danger">
-                                        Delete Account</a>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" class="button-justify"><span
-                                    class="glyphicon glyphicon-file">
-                            </span>Reports</a>
-                    </h4>
-                </div>
-                <div id="collapseFour" class="panel-collapse collapse">
-                    <div class="panel-body">
-                        <table class="table">
-                            <tr>
-                                <td>
-                                    <span class="glyphicon glyphicon-usd"></span><a href="http://www.jquery2dotnet.com">Sales</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span class="glyphicon glyphicon-user"></span><a
-                                            href="http://www.jquery2dotnet.com">Customers</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span class="glyphicon glyphicon-tasks"></span><a
-                                            href="http://www.jquery2dotnet.com">Products</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span class="glyphicon glyphicon-shopping-cart"></span><a
-                                            href="http://www.jquery2dotnet.com">Shopping Cart</a>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
+            @empty
+                <p>CREATE A LIST NO</p>
+            @endforelse
         </div>
     </div>
+
 
     <div>
         <ul>
