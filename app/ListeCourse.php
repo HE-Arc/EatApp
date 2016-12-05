997f7cfd706ee0c54d52c4c8638a9c8623846544
sarahgroell <sarah.groell@he-arc.ch>
2016-12-05 13:49:32 +0100

    modification du model ListeCourse

--- app/ListeCourse.php
+++ app/ListeCourse.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class ListeCourse extends Model
 {
     //
+    protected $table = 'liste_courses';
+
+    public function store(Request $request)
+    {
+        $listeCourse = new ListeCourse();
+        $listeCourse->nom = $request->nom;
+        $listeCourse->save();
+    }
+
+    public function updateListeCourse($id,$nom)
+    {
+        $listeCourse = ListeCourse::find($id);
+        $listeCourse->nom = $nom;
+        $listeCourse->save();
+    }
+
 }
